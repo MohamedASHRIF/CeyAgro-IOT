ec637f378c40ddb4160fe817e2b1c3b31f001741
Mohamed Ashrif <mohamedashrif72@gmail.com>
2025-04-15 12:27:11 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   // Connect Kafka microservice
-  app.connectMicroservice(kafkaConfig);
+  // app.connectMicroservice(kafkaConfig);
   
   await app.startAllMicroservices();
   await app.listen(process.env.PORT || 3000);
