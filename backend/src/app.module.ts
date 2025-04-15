e1a1f3bf4ba4cde6cb0d42411459bc26db9dda65
Mohamed Ashrif <mohamedashrif72@gmail.com>
2025-04-15 12:26:01 +0530

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { DeviceModule } from './device/device.module';
 
 @Module({
@@ -8,6 +9,7 @@ import { DeviceModule } from './device/device.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    MongooseModule.forRoot(process.env.MONGO_URI), // MongoDB connection
     DeviceModule,
   ],
 })
