386ade3f84dbb5e14cc17d788f3424b63ec8eb87
Mohamed Ashrif <mohamedashrif72@gmail.com>
2025-04-15 12:21:48 +0530

    Update device.module.ts

--- backend/src/device/device.module.ts
+++ backend/src/device/device.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DeviceController } from './device.controller';
 import { DeviceService } from './device.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DeviceData, DeviceDataSchema } from './schemas/device.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: DeviceData.name, schema: DeviceDataSchema }]),
+  ],
   controllers: [DeviceController],
   providers: [DeviceService],
 })
