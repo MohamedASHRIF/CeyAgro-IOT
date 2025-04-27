f051b31372f83908bf597fa4d9d44da5b974fc37
thavishiminolka <155363547+thavishiminolka@users.noreply.github.com>
2025-04-27 21:00:34 +0530

    Update providers.tsx

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,14 +1,9 @@
 "use client";
 
 import { UserProvider } from "@auth0/nextjs-auth0/client";
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
+
 import React from "react";
->>>>>>> Stashed changes
-=======
-import React from "react";
->>>>>>> Stashed changes
+
 import { ReactNode } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
