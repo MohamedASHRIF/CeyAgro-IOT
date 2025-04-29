8e72182d92058a72129e2054159fe83cd66a5b2c
AnjanaNimesh <anjananimesh22@gmail.com>
2025-04-30 00:18:15 +0530

    update analytics

--- analytic-api/src/main.ts
+++ analytic-api/src/main.ts
@@ -1,22 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { kafkaConfig } from './config/kafka.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://192.168.1.6:3000'],// Your Next.js frontend
+    origin: ['http://localhost:3000'],// Your Next.js frontend
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
   });
 
+  
+  // Set up global validation pipe
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   // Connect Kafka microservice
-  app.connectMicroservice(kafkaConfig)
-  await app.startAllMicroservices();
+  // app.connectMicroservice(kafkaConfig)
+  // await app.startAllMicroservices();
   
-  const port = process.env.PORT || '0.0.0.0';
+  const port = process.env.PORT|| 3001;
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
