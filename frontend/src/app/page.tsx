39ff249c0bd5e74d5e8bf119f1bacef7ab71099a
Mohamed Ashrif <mohamedashrif72@gmail.com>
2025-04-29 13:52:50 +0530

    added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,7 +7,7 @@
 "use client";
 import { redirect } from "next/navigation";
 import { useUser } from "@auth0/nextjs-auth0/client";
-import { redirect } from 'next/navigation'
+
 
 export default function Page() {
   const { user, isLoading } = useUser();
