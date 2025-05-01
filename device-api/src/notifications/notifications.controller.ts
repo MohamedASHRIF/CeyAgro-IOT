f04360ee43fd6ab4cb8745d7936c9620038c27d4
thavishiminolka <155363547+thavishiminolka@users.noreply.github.com>
2025-05-02 01:35:06 +0530

    changes

--- device-api/src/notifications/notifications.controller.ts
+++ device-api/src/notifications/notifications.controller.ts
@@ -7,12 +7,14 @@ export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
   @Get(':userId')
+  //retrieve all notifications for a specific user
   async findAllByUserId(
     @Param('userId') userId: string,
   ): Promise<Notification[]> {
     return this.notificationsService.findAllByUserId(userId);
   }
 
+  //DELETE requests to remove a specific notification by its ID
   @Delete(':id')
   async deleteNotification(@Param('id') id: string): Promise<void> {
     return this.notificationsService.deleteNotification(id);
--- device-api/src/users/users.service.ts
+++ device-api/src/users/users.service.ts
@@ -7,6 +7,7 @@ import { User } from './schemas/user.schema';
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
+  // Updates or creates a user's FCM token in the database
   async updateFcmToken(userId: string, fcmToken: string | null): Promise<void> {
     console.log(`Updating FCM token for user ${userId}:`, fcmToken);
     const result = await this.userModel
@@ -19,6 +20,7 @@ export class UsersService {
     console.log('MongoDB update result:', result);
   }
 
+  //Retrieves the FCM token for a given user
   async getFcmToken(userId: string): Promise<string | null> {
     const user = await this.userModel.findOne({ userId }).exec();
     return user?.fcmToken || null;
