986c6dcfaeebc1fffe573c81a7be8444e866c15d
NavoAma-Perera <155376266+NavoAma-Perera@users.noreply.github.com>
2025-07-16 01:58:49 +0530

    Update device-user.schema.ts

--- device-api/src/device-user/schemas/device-user.schema.ts
+++ device-api/src/device-user/schemas/device-user.schema.ts
@@ -1,3 +1,45 @@
+// // schemas/device-user.schema.ts
+// import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+// import { Document } from 'mongoose';
+
+// @Schema({ timestamps: true })
+// export class DeviceUser {
+//   @Prop({ required: true })
+//   userId: string;
+
+//   @Prop({ required: true })
+//   deviceId: string;
+
+//   @Prop({ required: true })
+//   deviceName: string;
+
+//   @Prop()
+//   description: string;
+
+//   @Prop()
+//   deviceImage: string;
+
+  
+//   @Prop()
+//   location: string;
+
+//   @Prop()
+//   deviceType: string;
+
+//   @Prop()
+//   serialNumber: string;
+
+//   @Prop()
+//   measurementUnit: string;
+
+//   @Prop()
+//   measurementParameter: string;
+// }
+
+// export type DeviceUserDocument = DeviceUser & Document;
+// export const DeviceUserSchema = SchemaFactory.createForClass(DeviceUser);
+
+
 // schemas/device-user.schema.ts
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
@@ -5,7 +47,7 @@ import { Document } from 'mongoose';
 @Schema({ timestamps: true })
 export class DeviceUser {
   @Prop({ required: true })
-  userId: string;
+  email: string;
 
   @Prop({ required: true })
   deviceId: string;
